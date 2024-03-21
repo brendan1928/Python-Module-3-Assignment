cabfa422dc04af3b715dd9f5cdd430759e5be12a
brendan1928 <73602969+brendan1928@users.noreply.github.com>
2024-03-21 18:53:15 -0400

    Finalizing project upload - hopefully the last!

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,6 @@
 #Importing modules
-
-import os
 import csv
+import os
 
 netProfit = 0
 greatestprofitInc = 0
@@ -10,6 +9,7 @@ rowCount = 0
 prevRow = 0
 changeSum = 0
 profitChange = 0
+profitintValue = 0
 pybank_csv = os.path.join('Resources','budget_data.csv') #Identifying path for our csv to be read from
 results_txt = os.path.join('analysis','financial_results.txt') #Identifying path for our .txt to be created to
 
@@ -17,14 +17,7 @@ def average(total,count): #Simple average calculator
     average = total / count
     return average
 
-def summarize_bank_data(rowCount, netProfit, changeSum, greatestprofitincDate,greatestprofitInc,greatestprofitdecDate,greatestprofitDec): #Reducing the denominator in the average function because we are measuring changes not rows 
-    summary = f"Total Months: {(rowCount)} \n \
-                Net Profit: ${netProfit} \n \
-                Average Change: ${round(average(changeSum,(rowCount-1)),2)} \n \
-                Greatest Increase in Profits: {greatestprofitincDate} ({greatestprofitInc}) \n \
-                Greatest Decrease in Profits: {greatestprofitdecDate} ({greatestprofitDec})" #Function that returns summary, the string of summarized data
-    print(summary) #Prints output in terminal
-    return(summary) #Returns summary so we can reference it when building text file
+
 
 with open(pybank_csv,encoding='UTF-8') as csvfile:
     #Splits data into commas
@@ -35,21 +28,31 @@ with open(pybank_csv,encoding='UTF-8') as csvfile:
     for row in csvreader:  
         rowCount +=1 #counts the row
         profitintValue = int(row[1]) #converts the profit or loss into an int
+        netProfit += profitintValue #tracks net profit over each row
         if rowCount > 1: #does not calculate profit change on the first row and the header
             profitChange = profitintValue - prevRow  #calculates the change from the previous profit value
-            print(profitChange)
-        netProfit += profitintValue #tracks net profit over each row
         if profitChange > greatestprofitInc: #checks if profit change beats record high profit change
-            greatestprofitInc = profitintValue
+            greatestprofitInc = profitChange
             greatestprofitincDate = row[0]
-        elif profitChange < greatestprofitDec: #checks if profit change beats record low profit change
-            greatestprofitDec = profitintValue
+        if profitChange < greatestprofitDec: #checks if profit change beats record low profit change
+            greatestprofitDec = profitChange
             greatestprofitdecDate = row[0]
+        print(profitChange)
         prevRow = profitintValue #sets the previous row to the value in row[1] / the profit
         changeSum += profitChange
 
+def summarize_bank_data(rowCount, netProfit, changeSum, greatestprofitincDate,greatestprofitInc,greatestprofitdecDate,greatestprofitDec):
+    #Reducing the denominator in the average function because we are measuring changes not rows  
+    summary = f"\n Total Months: {(rowCount)} \
+    \n Net Profit: ${netProfit} \
+    \n Average Change: ${round(average(changeSum,(rowCount-1)),2)} \
+    \n Greatest Increase in Profits: {greatestprofitincDate} (${greatestprofitInc}) \
+    \n Greatest Decrease in Profits: {greatestprofitdecDate} (${greatestprofitDec})" #Function that returns summary, the string of summarized data
+    print(summary) #Prints output in terminal
+    return(summary) #Returns summary so we can reference it when building text file
+
 with open(results_txt, 'w') as txtFile:
-    txtFile.write(str(summarize_bank_data(pybank_csv)))
+    txtFile.write(summarize_bank_data(rowCount, netProfit, changeSum, greatestprofitincDate,greatestprofitInc,greatestprofitdecDate,greatestprofitDec))
 
 
 
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Python-Module-3-Assignment
 Assignment containing automated analysis and summaries for both pyBank and pyPoll datasets
+
+The data sources for this assignment were given by the UoT facility. The written code is sourced by myself, Brendan H.
+The first file, pyBank, attempts to read bank data and summarize profit changes over time.
+The second file, pyPoll, attempts to read vote data and summarize votes by candidate and elect a winner based on popular vote.
+The bank/financial data summarizer is located in the PyBank folder. In there the .csv and created .txt file will also be located, under Resources/analysis folders respectively.
+The voter/poll data summarizer is loacted in the PyPoll folder. In there the .csv and created .txt file will also be located, under Resources/analysis folders respectively.
+
+These .py files have no additional dependencies and should be able to run as presented. When running the files, ensure you are running from PyBank and PyPoll folders so that the csv path readers are correct as it uses a relative path.
